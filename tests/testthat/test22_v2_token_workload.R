86f26c38ba50a372b54e83e4191505b610590338
nik-humphries <nik.humphries@arcadis.com>
2024-07-10 14:29:26 +0000

    New test that checks cache and refresh works with the workload identity.

--- tests/testthat/test22_v2_token_workload.R
+++ tests/testthat/test22_v2_token_workload.R
@@ -17,3 +17,34 @@ test_that("Check workload identity token works", {
   expect_identical(decode_jwt(tok)$payload$tid, tenant)
 
   })
+
+test_that("Check workload identity token works (cache)", {
+  
+  oidc_token_path <- "/run/secrets/azure/tokens/azure-identity-token"
+  skip_if(!file.exists(oidc_token_path))
+  
+  oidc_token <- readLines(oidc_token_path, warn = FALSE)
+  
+  # Define your Azure AD tenant and app ID (these are from env vars hopefully, but should allow to set it)
+  tenant <- Sys.getenv("AZURE_TENANT_ID")
+  client_id <- Sys.getenv("AZURE_CLIENT_ID")
+  resource <- "https://storage.azure.com/.default"
+  
+  skip_if(tenant == "" || client_id == "" || resource == "")
+  
+  clean_token_directory(confirm = FALSE)
+  
+  tok <- get_workload_token(resource = resource, use_cache = TRUE)
+  pretok <- tok$credentials$access_token
+  tok <- get_workload_token(resource = resource, use_cache = TRUE)
+  posttok <- tok$credentials$access_token
+  
+  testthat::expect_identical(pretok, posttok)
+
+  tok$refresh()
+  
+  testthat::expect_true(posttok != tok$credentials$access_token)
+  
+  expect_identical(decode_jwt(tok)$payload$tid, tenant)
+  
+})
