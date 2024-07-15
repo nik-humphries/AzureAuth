61e5ad55b9013f74f94452fbca227ed6f40addfe
Nik <nik.humphries@arcadisgen.com>
2024-07-15 13:53:54 +0100

    Replace hardcoded variable path with value from env var

--- R/workload_token.R
+++ R/workload_token.R
@@ -1,7 +1,7 @@
-#' @param oidc_token_path Path to the workload identity injected OIDC token, defaults to "/run/secrets/azure/tokens/azure-identity-token" for aks
+#' @param oidc_token_path Path to the workload identity injected OIDC token, defaults to Sys.getenv("AZURE_FEDERATED_TOKEN_FILE", unset = "/run/secrets/azure/tokens/azure-identity-token") for aks
 #' @rdname get_azure_token
 #' @export
-get_workload_token <- function(resource, tenant = Sys.getenv("AZURE_TENANT_ID"), app = Sys.getenv("AZURE_CLIENT_ID"), oidc_token_path = "/run/secrets/azure/tokens/azure-identity-token", password=NULL, username=NULL, certificate=NULL, auth_type=NULL,
+get_workload_token <- function(resource, tenant = Sys.getenv("AZURE_TENANT_ID"), app = Sys.getenv("AZURE_CLIENT_ID"), oidc_token_path = Sys.getenv("AZURE_FEDERATED_TOKEN_FILE", unset = "/run/secrets/azure/tokens/azure-identity-token"), password=NULL, username=NULL, certificate=NULL, auth_type=NULL,
                                aad_host="https://login.microsoftonline.com/", version=2,
                                authorize_args=list(), token_args=list(),
                                use_cache=NULL, on_behalf_of=NULL, auth_code=NULL, device_creds=NULL)
